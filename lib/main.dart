c261c6101aa00366b0d2279f9f94a93d74b7b790
Sanuda de silva <sanudadesilva2003@gmail.com>
2025-03-27 12:55:28 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:quizgenie/interface/login.dart'; // Ensure this file exists
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
