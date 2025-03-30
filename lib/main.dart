b67cc7f826fd2ba7d157d27c86b2fed6227dedd0
Malith Adikaram <malithadikaram1234@gmail.com>
2025-03-30 16:56:38 +0530

    Change to Feed screen

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: FeedScreen());
   }
 }
